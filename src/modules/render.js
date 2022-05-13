6983a8ce04442def21f036ff8a8c53c561206f7f
lUCI <lusindisontanjana@gmail.com>
2022-05-13 19:08:28 +0200

    Add css styles

--- src/modules/render.js
+++ src/modules/render.js
@@ -2,7 +2,7 @@ const render = (list) => {
   const score = document.querySelector('.scores__list');
   score.innerHTML = '';
   list.forEach((element) => {
-    score.innerHTML += `<li class="scores__list--item">${element.user}: ${element.score}</li>`;
+    score.innerHTML += `<li class="scores__list--item">${element.user}:  ${element.score}</li>`;
   });
 };
 
